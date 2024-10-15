679ab5327dff4b3b482c326039c2d1e0c62d3ed9
Sebast000 <163167593+Sebast000@users.noreply.github.com>
2024-10-15 22:47:33 +0000

    Update BibliothecaireDashboard.java

--- src/BibliothecaireDashboard.java
+++ src/BibliothecaireDashboard.java
@@ -1,25 +1,24 @@
 import javax.swing.*;
 
 public class BibliothecaireDashboard extends JFrame {
+    private Bibliotheque bibliotheque;
 
-    public BibliothecaireDashboard() {
+    public BibliothecaireDashboard(Bibliotheque bibliotheque) {
+        this.bibliotheque = bibliotheque;
         setTitle("Tableau de bord - Bibliothécaire");
-        setSize(600, 400);
+        setSize(400, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
 
         JButton addBookButton = new JButton("Ajouter un livre");
+        addBookButton.addActionListener(e -> new AddBookForm(bibliotheque).setVisible(true));
+
         JButton removeBookButton = new JButton("Supprimer un livre");
+        removeBookButton.addActionListener(e -> new RemoveBookForm(bibliotheque).setVisible(true));
 
         JPanel panel = new JPanel();
         panel.add(addBookButton);
         panel.add(removeBookButton);
 
         add(panel);
-        setVisible(true);
-
-        // Actions des boutons (ajout et suppression de livres)
-        addBookButton.addActionListener(e -> new AddBookForm());
-        removeBookButton.addActionListener(e -> new RemoveBookForm());
     }
 }
