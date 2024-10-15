be9ec93af10f42c4cee54b7957c9c94308303638
Sebast000 <163167593+Sebast000@users.noreply.github.com>
2024-10-15 21:49:03 +0000

    Create Utilisateur.java

---
+++ src/Utilisateur.java
@@ -0,0 +1,13 @@
+public class Utilisateur {
+    protected String nomUtilisateur;
+    protected String motDePasse;
+
+    public Utilisateur(String nomUtilisateur, String motDePasse) {
+        this.nomUtilisateur = nomUtilisateur;
+        this.motDePasse = motDePasse;
+    }
+
+    public String getNomUtilisateur() {
+        return nomUtilisateur;
+    }
+}
